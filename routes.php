0a55b56dc5b958950049feb3d442fdce19ac4f9b
Saifur Rahman Mohsin <mohsin92@me.com>
2015-09-14 19:57:02 +0530

    Updates
    Pushed the final version of plugin to repo.

--- routes.php
+++ routes.php
@@ -1,6 +1,9 @@
 <?php
 
 use RainLab\User\Models\Settings as UserSettings;
+use Mohsin\MediScan\Models\Prop;
+use Mohsin\MediScan\Models\Patient;
+use Mohsin\MediScan\Models\Scan;
 
 Route::get('/authtest', array('before' => 'auth.basic', function()
 {
@@ -15,6 +18,46 @@ Route::get('/authtest', array('before' => 'auth.basic', function()
 Route::get('/api', array('before' => 'auth.basic', function()
 {
       return Auth::getUser();
+}));
+
+Route::get('/prop', array('before' => 'auth.basic', function()
+{
+  if(!isset($_GET['id']))
+    return Response::json(array('result' => 'error', 'reason' => 'Id not specified' ), 200);
+  try {
+    $data = Prop::where('id','=',$_GET['id'])->get(['type','prop_id'])[0];
+  } catch (Exception $ex) {
+    return Response::json(array('result' => 'error', 'reason' => 'Invalid Prop' ), 200);
+  }
+  if($data != null)
+    return json_encode($data);
+}));
+
+Route::get('/patient', array('before' => 'auth.basic', function()
+{
+  if(!isset($_GET['id']))
+    return Response::json(array('result' => 'error', 'reason' => 'Id not specified' ), 200);
+  try {
+    $data = Patient::where('id','=',$_GET['id'])->get()[0];
+  } catch (Exception $ex) {
+    return Response::json(array('result' => 'error', 'reason' => 'Invalid Prop' ), 200);
+  }
+  if($data != null)
+    return json_encode($data);
+}));
+
+Route::get('/scan', array('before' => 'auth.basic', function()
+{
+  if(!isset($_GET['id']))
+    return Response::json(array('result' => 'error', 'reason' => 'Id not specified' ), 200);
+  try {
+    $data = Scan::where('id','=',$_GET['id'])->get()[0];
+    $data['patient'] = Patient::where('id','=',$data -> original['patient_id'])->get()[0]['name'];
+  } catch (Exception $ex) {
+    return Response::json(array('result' => 'error', 'reason' => 'Invalid Prop' ), 200);
+  }
+  if($data != null)
+    return json_encode($data);
 }));
 
 Route::filter('auth.basic', function()
